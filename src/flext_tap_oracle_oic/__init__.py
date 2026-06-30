43d8d34e5885a0561452d5cb4c88d92734e03099
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-06-30 05:38:44 -0300

    Generate tap Oracle OIC lazy registry wrappers

--- src/flext_tap_oracle_oic/__init__.py
+++ src/flext_tap_oracle_oic/__init__.py
@@ -3,9 +3,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-from flext_core.lazy import build_lazy_import_map, install_lazy_exports
+from flext_core.lazy import install_lazy_exports
+from flext_meltano import d, e, h, r, s, x
 from flext_tap_oracle_oic.__version__ import (
     __author__,
     __author_email__,
@@ -16,85 +15,34 @@ from flext_tap_oracle_oic.__version__ import (
     __version__,
     __version_info__,
 )
+from flext_tap_oracle_oic._exports import FLEXT_TAP_ORACLE_OIC_LAZY_IMPORTS
 
-if TYPE_CHECKING:
-    from flext_meltano import d as d, e as e, h as h, r as r, s as s, x as x
-    from flext_tap_oracle_oic.api import (
-        FlextTapOracleOicService as FlextTapOracleOicService,
-        tap_oracle_oic as tap_oracle_oic,
-    )
-    from flext_tap_oracle_oic.cli import (
-        FlextTapOracleOicCli as FlextTapOracleOicCli,
-        main as main,
-    )
-    from flext_tap_oracle_oic.constants import (
-        FlextTapOracleOicConstants as FlextTapOracleOicConstants,
-        c as c,
-    )
-    from flext_tap_oracle_oic.models import (
-        FlextTapOracleOicModels as FlextTapOracleOicModels,
-        m as m,
-    )
-    from flext_tap_oracle_oic.protocols import (
-        FlextTapOracleOicProtocols as FlextTapOracleOicProtocols,
-        p as p,
-    )
-    from flext_tap_oracle_oic.settings import (
-        FlextTapOracleOicSettings as FlextTapOracleOicSettings,
-    )
-    from flext_tap_oracle_oic.typings import (
-        FlextTapOracleOicTypes as FlextTapOracleOicTypes,
-        t as t,
-    )
-    from flext_tap_oracle_oic.utilities import (
-        FlextTapOracleOicUtilities as FlextTapOracleOicUtilities,
-        u as u,
-    )
-_LAZY_IMPORTS = build_lazy_import_map(
-    {
-        ".api": (
-            "FlextTapOracleOicService",
-            "tap_oracle_oic",
-        ),
-        ".cli": (
-            "FlextTapOracleOicCli",
-            "main",
-        ),
-        ".constants": (
-            "FlextTapOracleOicConstants",
-            "c",
-        ),
-        ".models": (
-            "FlextTapOracleOicModels",
-            "m",
-        ),
-        ".protocols": (
-            "FlextTapOracleOicProtocols",
-            "p",
-        ),
-        ".settings": ("FlextTapOracleOicSettings",),
-        ".typings": (
-            "FlextTapOracleOicTypes",
-            "t",
-        ),
-        ".utilities": (
-            "FlextTapOracleOicUtilities",
-            "u",
-        ),
-        "flext_meltano": (
-            "d",
-            "e",
-            "h",
-            "r",
-            "s",
-            "x",
-        ),
-    },
+_LAZY_IMPORTS = FLEXT_TAP_ORACLE_OIC_LAZY_IMPORTS
+
+
+_EAGER_EXPORTS = (
+    __author__,
+    __author_email__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+    __version_info__,
+    d,
+    e,
+    h,
+    r,
+    s,
+    x,
 )
 
 
-__all__: tuple[str, ...] = (
+_PUBLIC_EXPORTS: tuple[str, ...] = (
+    "FlextOracleOicAuthenticator",
+    "FlextTapOracleOic",
     "FlextTapOracleOicCli",
+    "FlextTapOracleOicClient",
     "FlextTapOracleOicConstants",
     "FlextTapOracleOicModels",
     "FlextTapOracleOicProtocols",
@@ -102,6 +50,7 @@ __all__: tuple[str, ...] = (
     "FlextTapOracleOicSettings",
     "FlextTapOracleOicTypes",
     "FlextTapOracleOicUtilities",
+    "tap_oracle_oic",
     "__author__",
     "__author_email__",
     "__description__",
@@ -120,7 +69,6 @@ __all__: tuple[str, ...] = (
     "r",
     "s",
     "t",
-    "tap_oracle_oic",
     "u",
     "x",
 )
@@ -130,5 +78,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=__all__,
+    public_exports=_PUBLIC_EXPORTS,
 )
--- src/flext_tap_oracle_oic/_models/__init__.py
+++ src/flext_tap_oracle_oic/_models/__init__.py
@@ -3,8 +3,16 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
+if TYPE_CHECKING:
+    from flext_tap_oracle_oic._models.streams import (
+        ALL_STREAMS as ALL_STREAMS,
+        FlextTapOracleOicModelsStreams as FlextTapOracleOicModelsStreams,
+        th as th,
+    )
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".streams": (
