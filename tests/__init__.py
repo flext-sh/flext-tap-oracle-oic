2c7cbf9b30aeea63bc8118f8bde6c8a8578d1406
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 13:15:00 -0300

    fixes

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from flext_tap_oracle_oic import FlextTapOracleOicConstants
     from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
 
-    from . import unit as unit
+    from . import unit
     from .base import (
         TestsFlextTapOracleOicServiceBase,
         TestsFlextTapOracleOicServiceBase as s,
