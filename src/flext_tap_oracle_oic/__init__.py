e130cb0c7e70200004a7b2f869432e48a37402d5
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 23:52:41 -0300

    fixes

--- src/flext_tap_oracle_oic/__init__.py
+++ src/flext_tap_oracle_oic/__init__.py
@@ -20,7 +20,9 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_meltano import d, e, h, r, s, x
+    from flext_meltano import s
+
+    from flext_core import d, e, h, r, x
 
     from .__version__ import FlextTapOracleOicVersion
     from ._config import FlextTapOracleOicConfig, config
@@ -113,7 +115,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".tap_streams": ("FlextTapOracleOicPaginator",),
             ".typings": ("FlextTapOracleOicTypes", "t"),
             ".utilities": ("FlextTapOracleOicUtilities", "u"),
-            "flext_meltano": ("d", "e", "h", "r", "s", "x"),
+            "flext_core": ("d", "e", "h", "r", "x"),
+            "flext_meltano": ("s",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
